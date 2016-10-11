d46881740415a914110232708bf4f2eb25260f77
EEtancelin <etancelin.edouard@gmail.com>
2016-10-11 18:46:26 +0200

    Re implements routes.rb in order to follow Ror conventions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'payements/index'
 
-  get 'interventions/index'
-
+  resources :payements
+  resources :interventions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
